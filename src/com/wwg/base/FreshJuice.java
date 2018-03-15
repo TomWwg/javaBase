3fc69e9f5b149f3919928de80b7ee802f5b434d8
TomWwg <741889962@qq.com>
2018-03-15 15:07:46 +0800

    a little knowledge

---
+++ src/com/wwg/base/FreshJuice.java
@@ -0,0 +1,12 @@
+package com.wwg.base;
+
+/**
+ *
+ * @author wwg
+ * @date 2018/3/15
+ */
+public class FreshJuice {
+    enum FreshJuiceSize{ SMALL, MEDIUM, LARGE }
+    FreshJuiceSize size;
+}
+
---
+++ src/com/wwg/base/FreshJuiceTest.java
@@ -0,0 +1,13 @@
+package com.wwg.base;
+
+/**
+ *
+ * @author wwg
+ * @date 2018/3/15
+ */
+public class FreshJuiceTest {
+    public static void main(String[] args) {
+        FreshJuice juice = new FreshJuice();
+        juice.size = FreshJuice.FreshJuiceSize.LARGE;
+    }
+}
