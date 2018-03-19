a0f23765869d366dc78737e9ebab54cb664329ea
TomWwg <741889962@qq.com>
2018-03-19 09:30:57 +0800

    构造方法

---
+++ src/com/wwg/base/Puppy.java
@@ -0,0 +1,20 @@
+package com.wwg.base;
+
+/**
+ * 在创建对象的时候，至少要调用一种构造方法。构造方法的名称必须与类名相同，一个类可以由多个构造方法。
+ * @author wwg
+ * @date 2018/3/15
+ */
+
+/**
+ *
+ */
+public class Puppy {
+    public Puppy(){
+    }
+
+    public Puppy(String name){
+        //这个构造器仅有一个参数：name
+    }
+}
+
